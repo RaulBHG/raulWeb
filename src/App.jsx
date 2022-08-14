88741d24e2b43c8b6ebfddaf75b107eded6bc642
RaulBHG <raulblazquezhernangomez@gmail.com>
2022-08-14 22:17:03 +0200

    all

--- src/App.jsx
+++ src/App.jsx
@@ -1,32 +1,18 @@
-import { useState } from 'react'
-import reactLogo from './assets/react.svg'
-import './App.css'
+import React from 'react'
+
+import Navbar from './components/navbar/Navbar'
+import Claim from './containers/claim/Claim'
+import Cta from './components/cta/Cta'
+import Feature from './components/feature/Feature'
 
 function App() {
-  const [count, setCount] = useState(0)
 
   return (
-    <div className="App">
-      <div>
-        <a href="https://vitejs.dev" target="_blank">
-          <img src="/vite.svg" className="logo" alt="Vite logo" />
-        </a>
-        <a href="https://reactjs.org" target="_blank">
-          <img src={reactLogo} className="logo react" alt="React logo" />
-        </a>
+    <div>
+      <div className='gradient_bg'>
+        <Navbar></Navbar>
+        <Claim></Claim>
       </div>
-      <h1>Vite + React</h1>
-      <div className="card">
-        <button onClick={() => setCount((count) => count + 1)}>
-          count is {count}
-        </button>
-        <p>
-          Edit <code>src/App.jsx</code> and save to test HMR
-        </p>
-      </div>
-      <p className="read-the-docs">
-        Click on the Vite and React logos to learn more
-      </p>
     </div>
   )
 }
---
+++ src/components/navbar/Navbar.jsx
@@ -0,0 +1,18 @@
+import React from 'react'
+import './navbar.css'
+
+const Navbar = () => {
+  return (
+    <div className='mainNavbar'>
+        <a href='#' className='LogoTitle'>Raúl <br/> BHG</a>
+        <div className='menuOptions'>
+          <a href='#home'>Home</a>
+          <a href='#home'>Home</a>
+          <a href='#home'>Home</a>
+          <a href='#home'>Home</a>
+        </div>
+    </div>
+  )
+}
+
+export default Navbar
